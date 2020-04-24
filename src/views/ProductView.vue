96128e5155e4b02bbecfe4e78321cd9ec125bfe2
Markus Voge <mvoge@posteo.de>
2020-04-24 13:54:52 +0200

    Add computed prefLocale addendum

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -19,7 +19,7 @@ import Footer from "@/components/Footer.vue";
  *************/
 import preferences from "@/assets/data/preferences.js";
 import productSearchResults from "@/assets/data/products.js";
-// import scolor from "@/assets/js/score-color.js";
+import scolor from "@/assets/js/score-color.js";
 
 // console.log(this.$route);
 // let prod_id = this.$route.query.id;
@@ -55,6 +55,12 @@ export default {
           break;
         }
       }
+      // if no preferred locale is present, fall back to first existing locale
+      if (Object.keys(prefLocale).length === 0 && Object.keys(this.product.locales).length > 0) { // is there easier way to access object length?
+        prefLocale = Object.values(this.product.locales)[0];
+      }
+      // if still undefined, create default empty locale
+      prefLocale = scolor.setDefaultIfEmpty(prefLocale);
       return prefLocale;
     }
   }
