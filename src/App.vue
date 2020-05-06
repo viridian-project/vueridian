8a44e4fab5a0df26511b96b3877ad765fe73efb1
Markus Voge <mvoge@posteo.de>
2020-05-06 20:06:33 +0200

    Add commented out hovercard close code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
+  <!-- <div id="app" @click="closeHovercards($event.target)"> -->
   <div id="app">
     <router-view />
   </div>
 </template>
 
 <style src="@/assets/css/main.css"></style>
+
+<script>
+// export default {
+//   methods: {
+//     closeHovercards: target => {
+//       if (
+//         !target.classList.contains("hovercard") &&
+//         !target.classList.contains("hovercard-container") &&
+//         !target.parentNode.classList.contains("hovercard-container") &&
+//         !target.parentNode.parentNode.classList.contains("hovercard-container") &&
+//         !target.parentNode.parentNode.parentNode.classList.contains("hovercard-container")
+//       ) {
+//         // close all hovercards
+//         let hcards = document.getElementsByClassName("hovercard");
+//         for (let hcard of hcards) {
+//           hcard.style.display = "none";
+//         }
+//       }
+//     }
+//   }
+// };
+</script>
